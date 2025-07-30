969e08540439ad6aa319a8a7a5e320b517668948
Pedro Oliveira <pedrogagodev@gmail.com>
2025-07-29 21:05:25 -0300

    feat: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import './App.css'
+import './index.css'
 
 function App() {
   return (
-    <div>
+    <div className="h-screen w-screen bg-[#121212] text-white flex items-center justify-center">
       <h1>Hello World</h1>
     </div>
   )
