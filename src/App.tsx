69deafbc1951cd319e303127baf60bec0c899f37
Pedro Oliveira <pedrogagodev@gmail.com>
2025-07-29 21:12:47 -0300

    run biome lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './index.css'
+import "./index.css";
 
 function App() {
-  return (
-    <div className="h-screen w-screen bg-[#121212] text-white flex items-center justify-center">
-      <h1>Hello World</h1>
-    </div>
-  )
+	return (
+		<div className="flex h-screen w-screen items-center justify-center bg-[#121212] text-white">
+			<h1>Hello World</h1>
+		</div>
+	);
 }
 
-export default App
+export default App;
