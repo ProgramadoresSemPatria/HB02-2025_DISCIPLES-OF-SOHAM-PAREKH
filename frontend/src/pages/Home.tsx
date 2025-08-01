56fc47608ece950379bbfc289bbe5ce518f7535b
Igor Moura <igor.oliveira.moura@gmail.com>
2025-08-01 18:41:21 -0300

    feat: home w/ all sections

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,9 @@
 
 import Header from "@/components/Header/Header";
+import CallToActionSection from "@/components/Home/CallActionSection";
+import FeaturesSection from "@/components/Home/FeaturesSection";
+import HeroSection from "@/components/Home/HeroSection";
+import TutorialSection from "@/components/Home/TutorialSection";
 import { UserButton } from "@clerk/clerk-react";
 
 
@@ -8,8 +12,11 @@ export function Home() {
 	return (
 		<div>
 			<Header/>
+			<HeroSection/>
 			<UserButton />
-          
+			<FeaturesSection />
+			<TutorialSection/>
+			<CallToActionSection/>
             
 		</div>
 	);
