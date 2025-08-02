6b4ca1f56ab4dafd15da3ff193f10eae82bbb684
Igor Moura <igor.oliveira.moura@gmail.com>
2025-08-02 11:20:18 -0300

    refactor: changed generate plan function

--- frontend/src/components/Buttons/GeneratePlanButton.tsx
+++ frontend/src/components/Buttons/GeneratePlanButton.tsx
@@ -1,15 +1,11 @@
-import { useNavigate } from "react-router";
+interface GeneratePlanButtonProps {
+  onGenerate: () => void;
+}
 
-const GeneratePlanButton = () => {
-  const navigate = useNavigate();
-
-  const generatePlan = () => {
-    navigate("/generate-plan");
-  };
-
+const GeneratePlanButton = ({ onGenerate }: GeneratePlanButtonProps) => {
   return (
     <button
-      onClick={generatePlan}
+      onClick={onGenerate}
       className="bg-blue-400 hover:bg-blue-500 text-white font-semibold px-10 py-2 rounded-md flex items-center justify-center gap-2 shadow-md transition w-full sm:w-auto"
     >
       Generate My Plan
