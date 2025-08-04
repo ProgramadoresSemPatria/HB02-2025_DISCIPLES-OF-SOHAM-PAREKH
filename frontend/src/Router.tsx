828ec024295faf286f905864fa8fd96b3c007ca7
Pedro Oliveira <pedrogagodev@gmail.com>
2025-08-04 20:54:50 -0300

    feat: update Router component to remove outdated pages and add new routes for vacation and relocation plans, enhancing navigation and user experience

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,17 +1,24 @@
 import { Home } from "./pages/Home";
 import { Routes, Route } from "react-router";
 import { AuthPage } from "./pages/AuthPage";
-import ChosenCountryToTravelPage from "./pages/ChosenCountryToTravelPage";
-import ChosenCountryRelocate from "./pages/ChosenCountryRelocate";
-
+import VacationResultPage from "./pages/VacationResultPage";
+import RelocationResultPage from "./pages/RelocationResultPage";
+import ListTravelPlansPage from "./pages/ListTravelPlansPage";
+import PlanFormPage from "./pages/PlanFormPage";
+import HeaderDestination from "./layouts/HeaderDestination";
 
 export function Router() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/authenticate" element={<AuthPage />} />
-			<Route path="/country-travel" element={<ChosenCountryToTravelPage/>}/>
-			<Route path="/country-relocate" element={<ChosenCountryRelocate/>}/>
+
+			<Route element={<HeaderDestination />}>
+				<Route path="/my-plans" element={<ListTravelPlansPage />} />
+				<Route path="/plan-form" element={<PlanFormPage />} />
+				<Route path="/vacation-plan/:id" element={<VacationResultPage />} />
+				<Route path="/relocation-plan/:id" element={<RelocationResultPage />} />
+			</Route>
 		</Routes>
 	);
 }
